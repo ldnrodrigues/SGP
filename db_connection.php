b99b77c357afe71a03df4d49d8dfb831847f5ace
Leonardo Davi <140842359+ldnrodrigues@users.noreply.github.com>
2024-05-03 21:52:25 -0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $database = "sgp_database";
 
-// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
